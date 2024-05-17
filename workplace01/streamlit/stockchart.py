2bccf4f74e04ba11891acd5a37b863764c833c78
Yoonseoparkk <patpark97@gmail.com>
2024-05-17 17:33:25 +0900

    streamlit 실습

---
+++ workplace01/streamlit/stockchart.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import FinanceDataReader as fdr
+import datetime
+
+# import bs4
+# import plotly
+
+# Finance Data Reader
+# https://github.com/financedata-org/FinanceDataReader
+
+date = st.date_input("조회 시작일을 선택해 주세요", datetime.datetime(2024, 5, 17))
+
+code = st.text_input("종목코드", value="", placeholder="종목코드를 입력해 주세요")
+
+if code and date:
+    df = fdr.DataReader(code, date)
+    data = df.sort_index(ascending=True).loc[:, "Close"]
+    st.line_chart(data)
